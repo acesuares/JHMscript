b2ab8495a40103e4bdb67a7d6f1cd2747cdc2f43
Ace Suares <ace@suares.an>
2021-09-21 10:04:41 -0400

    improvements

--- download.rb
+++ download.rb
@@ -5,10 +5,12 @@ require "httparty"
 require "json"
 require 'open-uri'
 
+base_dir = "/media/pi/CA3A9E753A9E5DEF/"
+
 puts "making the dirs..."
 %w(html html/videos html/thumbnails).each do |dir|
   puts "  #{dir}"
-  Dir.mkdir(dir) unless File.exists?(dir)
+  Dir.mkdir(base_dir + dir) unless File.exists?(base_dir + dir)
 end
 
 puts "load the templates..."
@@ -17,7 +19,7 @@ player_template  = IO.read("player.html.erb")
 
 puts "get the data from the server..."
 base_url = "https://jhm.suares.com"
-base_url = "http://localhost:3000"
+#base_url = "http://localhost:3000"
 api_key = 'woogekiemeec4Phoh7ahz8quahv9gi'
 tv = 1
 
@@ -28,21 +30,24 @@ puts "  HTTP: #{response.code}"
 
 puts "write html/chooser.html..."
 @topics = response.parsed_response["topics"]
-IO.write("html/chooser.html", ERB.new(chooser_template).result(binding))
+IO.write(base_dir + "html/chooser.html", ERB.new(chooser_template).result(binding))
 
 @movies = @topics.map{|topic| topic["tv_movies"]}.flatten.uniq
 @movies.each do |movie|
   @movie = movie
   puts "write html/player-#{movie['id']}.html..."
-  IO.write("html/player-#{movie['id']}.html", ERB.new(player_template).result(binding))
+  IO.write(base_dir + "html/player-#{movie['id']}.html", ERB.new(player_template).result(binding))
   movie_url = movie["movie"]["url"]
-  puts "  download #{base_url}#{movie_url}"
-  Down.download("#{base_url}#{movie_url}",
-          destination: "html/videos/#{movie['id']}.mp4")
-
+  if File.exist?(base_dir + "html/videos/#{movie['id']}.mp4")
+    puts "  already downloaded #{base_url}#{movie_url}"
+  else
+    puts "  download #{base_url}#{movie_url}"
+    Down.download("#{base_url}#{movie_url}",
+            destination: base_dir + "html/videos/#{movie['id']}.mp4")
+  end
 
   thumbnail_url = movie["thumbnail"]["url"]
   puts "  download #{base_url}#{thumbnail_url}"
   Down.download("#{base_url}#{thumbnail_url}",
-          destination: "html/thumbnails/#{movie['id']}.png")
+          destination: base_dir + "html/thumbnails/#{movie['id']}.png")
 end
